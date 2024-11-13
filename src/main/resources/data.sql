9584a038415d4a3903f853be83cd234441a55bc3
Giovanni-Morales-Camacho <103343387+Giovanni-Morales-Camacho@users.noreply.github.com>
2024-11-13 11:33:34 -0600

    Incluí un usuario Adm2 en el código de data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
 INSERT INTO app_usuario (username, password, role) VALUES ('user2', '$2a$10$uioWgiRiHRhA9erLswW9He39EV3PBcVd4wdmNcDsq1N3WHhvgHA8q', 'USER');
 INSERT INTO app_usuario (username, password, role) VALUES ('user1', '$2a$10$qKo4xv3RUlx8R8HQt0Pq7uI/xVJ687vDkG5XYb.QtJoRBEgXGqEzi', 'USER');
 INSERT INTO app_usuario (username, password, role) VALUES ('adm1', '$2a$10$qKo4xv3RUlx8R8HQt0Pq7uI/xVJ687vDkG5XYb.QtJoRBEgXGqEzi', 'ADMIN');
+INSERT INTO app_usuario (username, password, role) VALUES ('adm2', '$2a$10$qKo4xv3RUlx8R8HQt0Pq7uI/xVJ687vDkG5XYb.QtJoRBEgXGqEzi', 'ADMIN');
 /* al ejecutarse el main van a crear usuarios con contraseñas encriptadas
 ID      CONTRASEÑA      ENCRIPTACIÓN
 user2   password123     $2a$10$uioWgiRiHRhA9erLswW9He39EV3PBcVd4wdmNcDsq1N3WHhvgHA8q
